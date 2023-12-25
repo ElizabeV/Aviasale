ed846d0a5e1cb7dacbbae81da5befb04772424a4
Elizabeth Viricheva <liza.vir@ya.ru>
2023-12-25 21:15:24 +0300

    added 1st code

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int a = 13_676;
+        int b = 20;
+        int c = a / b;
+        System.out.println("Количество начисленных миль: " + c);
+    }
+}
